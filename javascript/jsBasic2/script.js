5eece1ec5f09fd55829049a3cbf8b2fbeaf119f9
aryanbhutani26 <159881202+aryanbhutani26@users.noreply.github.com>
2025-01-11 23:42:10 +0530

    Done

---
+++ javascript/jsBasic2/script.js
@@ -0,0 +1,20 @@
+// objects
+
+let obj = {
+    name: 'Aryan',
+    age: 20,
+    city: 'Delhi'
+}
+
+console.log(obj);
+
+let obj2 = {
+    name: 'Bhutani',
+    age: 20,
+    city: 'UP'
+}
+console.log(obj2);
+
+for(let i in obj){
+    console.log(obj[i]);
+}
