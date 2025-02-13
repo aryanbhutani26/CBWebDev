aa069b63e4739651eefb2e31559d8a0dc66d30e4
aryanbhutani26 <159881202+aryanbhutani26@users.noreply.github.com>
2025-02-13 09:47:48 +0530

    Old

---
+++ Nodejs2/server.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+// fs.writeFileSync('Helooo.txt' , 'hello world')
+
+
+const path= require('path')
+// let extension = path.extname('Helooo.txt')
+// let basename = path.basename('Helooo.txt')
+// console.log(extension)
+
+
+// console.log('base',basename);
+
